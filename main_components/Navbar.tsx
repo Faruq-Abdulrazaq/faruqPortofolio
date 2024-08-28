0e1a8a069b0701c19a93a68e58eb4e9ab399d21c
Faruq Taye Abdulrazaq <faruqtayeabdulrazaq@gmail.com>
2024-08-28 12:07:00 +0100

    nav

--- main_components/Navbar.tsx
+++ main_components/Navbar.tsx
@@ -17,11 +17,11 @@ const Navbar: React.FC<NavProps> = ({ scrollToSection, refs }) => {
     <div className="w-full flex flex-wrap justify-between items-center p-4">
       <div className="flex-shrink-0">
         <Image
-          className="ml-[-10px] lg:ml-32"
+          className="ml-[-10px] lg:ml-32 w-24 h-16 lg:w-auto lg:h-auto"
           src="/faruq_logo_main.png"
           alt="faruq logo"
-          width={130}
-          height={130}
+          width={100}
+          height={100}
         />
       </div>
 
